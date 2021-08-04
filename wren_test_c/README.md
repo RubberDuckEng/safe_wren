a39fa5d171efd7aa278e32e46c9b98578ae6b133
Eric Seidel <github@eseidel.com>
2021-08-03 20:35:56 -0700

    Copy in wren_c's wren_test
    This adds a (temporary?) copy of wren_c's wren_test so that I have something I can make build against wren_rust's c API.
    
    Right now this doesn't quite link, but it's also not that far off!
    https://gist.github.com/eseidel/91f84e51c4dcf433891ec2a53db01de0 is the burndown list of symbols to add.
    
    I also will probably comment out/remove .c files instead of adding apis on the path to making it link.
    
    The modification to cargo.toml was to tell cargo to build both a normal rust library and a c-style dynamic library (.so).
    
    The premade5.lua added seems to work and generated a valid Makefile.

---
+++ wren_test_c/README.md
@@ -0,0 +1,5 @@
+This is a copy of the .c files to build wren_test from wren_c.
+
+The eventual goal is to be able to build wren_c's wren_test using wren_rust
+as the wren library.  However to get there will require modifying wren_test
+some as we go.
---
+++ wren_test_c/premake5.lua
@@ -0,0 +1,34 @@
+workspace "wren"
+  configurations { "Release", "Debug" }
+  platforms { "64bit", "32bit" }
+  defaultplatform "64bit"
+  startproject "wren_test"
+
+  filter "configurations:Debug"
+    targetsuffix "_d"
+    defines { "DEBUG" }
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+
+project "wren_test"
+  kind "ConsoleApp"
+  language "C"
+  cdialect "C99"
+  targetdir "bin"
+  links { "wren_rust" }
+  libdirs { "../target/debug" }
+
+  files {
+    "main.c",
+    "test.c",
+    "test.h",
+    "api/*.c",
+    "api/*.h"
+  }
+
+  includedirs {
+    "../wren_c/src/include"
+  }
